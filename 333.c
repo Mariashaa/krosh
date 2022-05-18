0cb1b9fb9efaff06b703f0e076308337a9298883
Mariashaa <99796665+Mariashaa@users.noreply.github.com>
2022-05-18 13:38:40 +0500

    Update 333.c

--- 333.c
+++ 333.c
@@ -26,3 +26,26 @@ int main() {
 
     return 0;
 }
+
+#include <math.h>
+#include <stdio.h>
+int
+main()
+{
+    double h;
+    printf("h = ");
+    scanf("%lf", &h);
+    printf("%-17sf(x)\n", "x");
+    int step_h = (int)(2 / h);
+    double f;
+    for(-1.5 <= x && x <= 0.0)
+    {
+        double x = i * h;
+        if(0.0 <= x && x <= 1.5)
+            f = exp(sin(2 * x));
+        else
+            f = (x * x - 2 * x * x * x) * cos(x * x);
+        printf("x = %lf\t f(x) = %lf\n", x, f);
+    }
+    return 0;
+}
